383d1eb1a76b15fcf91ffb275851fa80c6f8ea39
Prithvi P Reddy <138385954+PrithviPReddy@users.noreply.github.com>
2025-09-21 10:41:17 +0530

    Update middleware.ts

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import {  authMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
