82853633178c2482466ee7fc7ba603c641f60bc3
Prithvi P Reddy <138385954+PrithviPReddy@users.noreply.github.com>
2025-09-21 10:33:59 +0530

    Update middleware.ts

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
@@ -6,19 +6,20 @@ const isPublicRoute = createRouteMatcher([
   '/signup(.*)',
   '/login/sso-callback(.*)',
   '/signup/sso-callback(.*)'
-])
+]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+  runtime: 'nodejs',
+};
